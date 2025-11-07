9e34ccd19ac6d1635db91ff0ce0a72cb0d673340
Shiva Bishwokarma <shivabishwokarma90@gmail.com>
2025-11-07 17:53:51 +0000

    conversions

---
+++ basicjs/conversion.js
@@ -0,0 +1,22 @@
+let age="20ab"
+
+console.log(typeof (age));
+let num=Number(age);
+console.log(typeof num);
+console.log(num);//not a number output
+//if boolen true=> 1
+//null=> 0
+//undefined,string=> NaN
+let login=1;
+let boollogin=Boolean(login);
+console.log(typeof (boollogin));
+// true=1; false=0
+// ""=> false 0
+//"shiva" => true
+let name="shiva";
+let isname=String(name);
+console.log(name);
+console.log(typeof isname);
+
+ 
+
