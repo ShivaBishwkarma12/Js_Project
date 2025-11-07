c039523218605c8668e1fd5674492c84abda21c5
Shiva Bishwokarma <shivabishwokarma90@gmail.com>
2025-11-07 16:57:38 +0000

    container config done

---
+++ basicjs/variables.js
@@ -0,0 +1,9 @@
+const accId="12344";
+let accemail="Shiva@gmail.com";
+var accpass="1234";//not prefered to use;
+console.log(accemail);
+
+accemail="Eren@gmail"
+accpass="3445"
+let accity="AOT"
+console.table([accemail,accpass,accity]);
