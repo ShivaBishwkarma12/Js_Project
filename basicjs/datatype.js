f26bf75ff3e084f81c59fd6cd12ecfed3130eaf4
Shiva Bishwokarma <shivabishwokarma90@gmail.com>
2025-11-07 17:31:01 +0000

    datatype

---
+++ basicjs/datatype.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+
+//alert(7 + 8);  used in browser not nodejs 
+//number=> 2 to the power 53
+//bigint 
+// string ""
+// boolean true/ false
+// null => standalone value
+//undefined=>
+// symbol => unique
+let name="shiva"
+let age=20
+let city="kathmandu"
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof city);
+
