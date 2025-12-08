edd9bd4155974fdd99ddc342dcf20e4fd47917e1
Shiva Bishwokarma <shivabishwokarma90@gmail.com>
2025-12-08 05:25:48 +0000

    cotainer config done

---
+++ basicjs/comparison.js
@@ -0,0 +1,17 @@
+/*let a=2>1;
+b=Boolean(a);
+console.log(b);
+console.log(5>4);*/
+
+//console.log("02" >1);
+console.log("2">1);//true
+
+console.log(null>0);//{comparison convert null to a number 0}
+console.log(null<0);
+console.log(null>=0);//true
+
+console.log(undefined==0);// >0r< all false
+
+console.log("2"===2);//strict equality operator
+
+
