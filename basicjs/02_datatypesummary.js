24242729d76f04f577aa1bf68273e3f4f94c329e
Shiva Bishwokarma <shivabishwokarma90@gmail.com>
2025-12-08 06:02:29 +0000

    container config done

---
+++ basicjs/02_datatypesummary.js
@@ -0,0 +1,25 @@
+//primitive 
+//string;number; boolean; null; undefined; Symbol; BigInt
+
+const numb=100;
+const percent=95.6;
+const isLoggedIn=false;
+const temperature=null;
+let userpass;
+const id=Symbol('521');
+const anotherid=Symbol('521');
+console.log(id===anotherid);
+const BigNumb= 5555533333333333333n; 
+console.log(BigNumb);
+
+//Non Primitive/reference
+// object; array; functions
+let OnePiece=["Luffy","Zoro","Sanji"];
+let mydata=
+{
+    id:"1",
+     name :"shiva",
+      number :2344,
+}
+console.log(typeof BigNumb);
+
