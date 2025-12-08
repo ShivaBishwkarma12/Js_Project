4a6534d921eac422fb326665fa1abc54a0826f17
Shiva Bishwokarma <shivabishwokarma90@gmail.com>
2025-12-08 05:09:06 +0000

    container config done

---
+++ basicjs/operation.js
@@ -0,0 +1,31 @@
+let val=4;
+let negval=-val;
+console.log(negval);
+console.log(2**4);//2^4
+let str1="hey";
+let str2=" shiva";
+let str3=str1 + str2;
+console.log(str3);
+console.log("1" + 2);//12
+console.log("1" + "2");//12
+console.log(1 + "2");//12
+console.log(1 +2 + "2");//32
+console.log(((3+2)*5));
+console.log(+true);//1
+console.log(-true);//-1
+//console.log(true+);//error
+console.log(+"");//0 tricky conversion
+let num1,num2,num3;
+nem1=num2=num3=4;//bad code readibility
+let gamecount=50;
+gamecount++;
+console.log(gamecount);
+
+
+
+
+
+
+
+
+
